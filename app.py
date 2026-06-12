9f8c37c7caca1647495740e179c1959eddd563d2
bitvvv76 <bitvvv76@gmail.com>
2026-06-12 10:09:24 +0400

    Add queries page to web panel

--- app.py
+++ app.py
@@ -1,7 +1,75 @@
 from flask import Flask
-from database import get_all_subscriptions
+from database import get_all_subscriptions, get_user_queries
 
 app = Flask(__name__)
+
+@app.route("/subscriptions")
+def subscriptions_page():
+
+    subscriptions = get_all_subscriptions()
+
+    html = """
+    <h1>Подписки Tender Helper</h1>
+
+    <a href="/">← Назад в Dashboard</a>
+
+    <hr>
+
+    <table border="1" cellpadding="10">
+    <tr>
+        <th>Категория</th>
+        <th>Регион</th>
+        <th>Бюджет</th>
+    </tr>
+    """
+
+    for sub in subscriptions:
+        html += f"""
+        <tr>
+            <td>{sub[2]}</td>
+            <td>{sub[3]}</td>
+            <td>{f"{sub[4]:,}".replace(",", " ")} ₽</td>
+        </tr>
+        """
+
+    html += "</table>"
+
+    return html
+
+@app.route("/queries")
+def queries_page():
+
+    queries = get_user_queries(1113849253, 20)
+
+    html = """
+    <h1>История запросов</h1>
+
+    <a href="/">← Назад в Dashboard</a>
+
+    <hr>
+
+    <table border="1" cellpadding="10">
+    <tr>
+        <th>Категория</th>
+        <th>Регион</th>
+        <th>Бюджет</th>
+        <th>Дата</th>
+    </tr>
+    """
+
+    for q in queries:
+        html += f"""
+        <tr>
+            <td>{q[0]}</td>
+            <td>{q[1]}</td>
+            <td>{q[2]}</td>
+            <td>{q[3]}</td>
+        </tr>
+        """
+
+    html += "</table>"
+
+    return html
 
 
 @app.route("/")
@@ -14,8 +82,8 @@ def index():
 
     <nav>
         <a href="/">Главная</a> |
-        <a href="/">Подписки</a> |
-        <a href="/">Запросы</a> |
+        <a href="/subscriptions">Подписки</a> |
+        <a href="/queries">Запросы</a>
         <a href="/">Тендеры</a> |
         <a href="/">Статистика</a>
     </nav>
